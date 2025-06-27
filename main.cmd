c7cebb167991e77e9ce3fd3fe5be1155c1cb5250
Exclavia <git@exclavia.network>
2025-06-27 05:27:56 -0400

    Init

---
+++ main.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
+set PATH=c:\users\trist\appdata\local\programs\python\python311;%PATH%
+set PYTHONHOME=C:\Users\trist\AppData\Local\Programs\Python\Python311
+set NUITKA_PYTHONPATH=C:\Users\trist\Coding\Offline-Repo\Satellite-Tracker;C:\Users\trist\AppData\Local\Programs\Python\Python311\DLLs;C:\Users\trist\AppData\Local\Programs\Python\Python311\Lib;C:\Users\trist\AppData\Local\Programs\Python\Python311;C:\Users\trist\AppData\Roaming\Python\Python311\site-packages;C:\Users\trist\AppData\Local\Programs\Python\Python311\Lib\site-packages;C:\Users\trist\AppData\Local\Programs\Python\Python311\Lib\site-packages\win32;C:\Users\trist\AppData\Local\Programs\Python\Python311\Lib\site-packages\win32\lib;C:\Users\trist\AppData\Local\Programs\Python\Python311\Lib\site-packages\Pythonwin;C:\Users\trist\AppData\Local\Programs\Python\Python311\Lib\site-packages\setuptools\_vendor
+"%~dp0main.exe" %*
