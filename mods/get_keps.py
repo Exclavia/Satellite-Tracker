b7f045ca8b5d7040f41b4175ec61ffec941ceedd
Exclavia <git@exclavia.network>
2025-06-27 12:00:18 -0400

    Last Sync: 2025-06-27 12:00 (Mobile)

--- mods/get_keps.py
+++ mods/get_keps.py
@@ -3,7 +3,7 @@ from skyfield.api import load
 
 # Downloads Keplarian(Keps) Elements from Celestrak, only if file hasn't been download yet
 # + only if previous file is more than max_days old (default 7.0)
-def get_keps(sat_group:str, file_format:str, max_days=7.0):
+def get_keps(sat_group:str, file_format:str, force_dl=False, max_days=7.0):
     """get_keps(sat_group: str -> Satellite group,
                file_format: str -> Format of the downloaded data,
                max_days: float -> Maximum amount of days before redownloading file (Default: 7.0)"""
@@ -18,7 +18,7 @@ def get_keps(sat_group:str, file_format:str, max_days=7.0):
         url_base = "https://celestrak.org/NORAD/elements/gp.php"
         url_opt = f"?GROUP={s_group}&FORMAT={kep_format}"
         url = url_base + url_opt
-        if not load.exists(file_path) or load.days_old(file_path) >= m_days:
+        if not load.exists(file_path) or load.days_old(file_path) >= m_days or force_dl:
             load.download(url, filename=file_path)
         return file_path
 
